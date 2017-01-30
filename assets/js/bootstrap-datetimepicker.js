05785a55429e0b9e455b4fe077c19adfc0b64c1a
Thomas <thomas@enizehates.de>
2017-01-30 16:29:46 +0100

    Update bootstrap-datetimepicker.js

--- assets/js/bootstrap-datetimepicker.js
+++ assets/js/bootstrap-datetimepicker.js
@@ -54,7 +54,7 @@
   function timeZoneAbbreviation() {
     var abbreviation, date, formattedStr, i, len, matchedStrings, ref, str;
     date = (new Date()).toString();
-    error.log(date+'  <----');
+    console.log(date+'  <----');
     formattedStr = ((ref = date.split('(')[1]) != null ? ref.slice(0, -1) : 0) || date.split(' ');
     if (formattedStr instanceof Array) {
       matchedStrings = [];
